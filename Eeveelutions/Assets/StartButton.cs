879e4f37aabe8eb89bb0abc28aeb0f3b0b762a68
Simone <ocvirks@mcmaster.ca>
2022-04-12 17:23:27 -0400

    setup initialized values

--- Eeveelutions/Assets/StartButton.cs
+++ Eeveelutions/Assets/StartButton.cs
@@ -3,12 +3,32 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour
-{
+public class StartButton : MonoBehaviour {
+
     [SerializeField] private string mainView = "MainView";
+    private int initFood = 0;
+    private int initHealth = 10;
+    private int initCurrency = 0;
+    private int initFire = 0;
+    private int initWater = 0;
+    private int initElectric = 0;
     
-    public void StartGame()
-    {
+    public void StartGame() {
+
+        SetDefaults();
         SceneManager.LoadScene(mainView);
+    
+    }
+
+    public void SetDefaults() {
+
+        PlayerPrefs.SetInt("Food", initFood);
+        PlayerPrefs.SetInt("Health", initHealth);
+        PlayerPrefs.SetInt("Currency", initCurrency);
+        PlayerPrefs.SetInt("Fire", initFire);
+        PlayerPrefs.SetInt("Water", initWater);
+        PlayerPrefs.SetInt("Electric", initElectric);
+        PlayerPrefs.Save();
+
     }
 }
