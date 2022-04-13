7b0231b2d06b1abb7e2f9db33a173c021200480d
bridelc <bridelcaitlin@gmail.com>
2022-04-12 21:22:18 -0400

    Inventory disappears when zero

--- Eeveelutions/Assets/Scripts/Stats/InventoryText.cs
+++ Eeveelutions/Assets/Scripts/Stats/InventoryText.cs
@@ -9,6 +9,10 @@ public class InventoryText : MonoBehaviour {
     public Text fireStoneText;
     public Text waterStoneText;
     public Text electricStoneText;
+    public GameObject foodItem;
+    public GameObject fireItem;
+    public GameObject waterItem;
+    public GameObject electricItem;
 
     public void Start() {
         
@@ -21,11 +25,46 @@ public class InventoryText : MonoBehaviour {
 
     public void Update() {
 
-        foodText.text = PlayerPrefs.GetInt("Food").ToString();
-        fireStoneText.text = PlayerPrefs.GetInt("Fire").ToString();
-        waterStoneText.text = PlayerPrefs.GetInt("Water").ToString();
-        electricStoneText.text = PlayerPrefs.GetInt("Electric").ToString();
-
+        if (PlayerPrefs.GetInt("Food") > 0)
+        {
+            foodItem.SetActive(true);
+            foodText.text = PlayerPrefs.GetInt("Food").ToString();
+        }
+        else
+        {
+            foodItem.SetActive(false);
+            foodText.text = "";
+        }
+        if (PlayerPrefs.GetInt("Fire") > 0)
+        {
+            fireItem.SetActive(true);
+            fireStoneText.text = PlayerPrefs.GetInt("Fire").ToString();
+        }
+        else
+        {
+            fireItem.SetActive(false);
+            fireStoneText.text = "";
+        }
+        if (PlayerPrefs.GetInt("Water") > 0)
+        {
+            waterItem.SetActive(true);
+            waterStoneText.text = PlayerPrefs.GetInt("Water").ToString();
+        }
+        else
+        {
+            waterItem.SetActive(false);
+            waterStoneText.text = "";
+        }
+        if (PlayerPrefs.GetInt("Electric") > 0)
+        {
+            electricItem.SetActive(true);
+            electricStoneText.text = PlayerPrefs.GetInt("Electric").ToString();
+        }
+        else
+        {
+            electricItem.SetActive(false);
+            electricStoneText.text = "";
+        }
     }
 
 }
