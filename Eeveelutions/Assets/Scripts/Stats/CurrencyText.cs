47f9c674cec7fbc5422292f09b60a2201bdb5bfe
namy2 <namy2@mcmaster.ca>
2022-04-12 22:14:59 -0400

    added dollar symbol beside currency

--- Eeveelutions/Assets/Scripts/Stats/CurrencyText.cs
+++ Eeveelutions/Assets/Scripts/Stats/CurrencyText.cs
@@ -4,18 +4,18 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class CurrencyText : MonoBehaviour {
-    
+
     public Text currencyText;
 
     void Start() {
 
-        currencyText.text = PlayerPrefs.GetInt("Currency").ToString();
-    
+        currencyText.text = "$ " + PlayerPrefs.GetInt("Currency").ToString();
+
     }
 
     void Update() {
 
-        currencyText.text = PlayerPrefs.GetInt("Currency").ToString();
+        currencyText.text = "$ " + PlayerPrefs.GetInt("Currency").ToString();
 
     }
 }
